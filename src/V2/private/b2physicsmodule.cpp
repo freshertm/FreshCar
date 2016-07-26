1baf8963470ac51f5a7f4dfb378704483e304e5f
Fresher TM <fresher.tm@gmail.com>
2016-07-26 11:36:54 +0700

    resolved conflicts

--- src/V2/private/b2physicsmodule.cpp
+++ src/V2/private/b2physicsmodule.cpp
@@ -1,6 +1,12 @@
 #include "b2physicsmodule.h"
 
-B2PhysicsModule::B2PhysicsModule()
+B2PhysicsModule::B2PhysicsModule():
+    _world(std::make_shared<b2World>(b2Vec2(0,-10)))
+{
+
+}
+
+B2PhysicsModule::~B2PhysicsModule()
 {
 
 }
--- src/V2/private/b2physicsmodule.h
+++ src/V2/private/b2physicsmodule.h
@@ -2,11 +2,14 @@
 #define B2PHYSICSMODULE_H
 
 #include "v2physicsmodule.h"
+#include <memory>
 #include "Box2D.h"
+#include <QThread>
 
 class V2Engine;
 class B2PhysicsModule: public V2PhysicsModule
 {
+    Q_OBJECT
 public:
     B2PhysicsModule();
     virtual ~B2PhysicsModule();
@@ -14,7 +17,8 @@ public:
     virtual bool stop(V2Engine*);
 
 private:
-
+    std::shared_ptr<b2World> _world;
+    QThread _thread;
 };
 
 #endif // B2PHYSICSMODULE_H
