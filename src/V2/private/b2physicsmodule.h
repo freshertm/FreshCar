e79dd99653247c8d2ab89f3799bc8854557880a4
FresherTM <Fresher.spam@gmail.com>
2016-07-26 08:01:46 +0700

    пилю потихоньку

--- src/V2/private/b2physicsmodule.h
+++ src/V2/private/b2physicsmodule.h
@@ -2,11 +2,19 @@
 #define B2PHYSICSMODULE_H
 
 #include "v2physicsmodule.h"
+#include "Box2D.h"
 
+class V2Engine;
 class B2PhysicsModule: public V2PhysicsModule
 {
 public:
     B2PhysicsModule();
+    virtual ~B2PhysicsModule();
+    virtual bool init(V2Engine*);
+    virtual bool stop(V2Engine*);
+
+private:
+
 };
 
 #endif // B2PHYSICSMODULE_H
