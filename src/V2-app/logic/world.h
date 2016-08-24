e44e1b7889cf0e9321390ada9473848021d2fd6a
Fresher TM <fresher.tm@gmail.com>
2016-08-24 19:55:17 +0700

    Пилю потихоньку

---
+++ src/V2-app/logic/world.h
@@ -0,0 +1,17 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <QObject>
+
+class World : public QObject
+{
+    Q_OBJECT
+public:
+    explicit World(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // WORLD_H
